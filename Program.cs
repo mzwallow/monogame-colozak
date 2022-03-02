887fc822cffb2353ddf08dcaab2d05e2fa723842
Punnarat Rattanapawan <punnrat_mos@hotmail.com>
2022-03-02 22:17:44 +0700

    :tada: Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Colozak
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Colozak())
+                game.Run();
+        }
+    }
+}
